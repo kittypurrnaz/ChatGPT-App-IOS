d78f6b676c3b800b346d60809a8a7d5e2290412e
Nur Nazifa Sarah Ahmad <115413009+kittypurrnaz@users.noreply.github.com>
2023-04-26 11:52:00 +0800

    Add files via upload

---
+++ chatGPT/chatGPT/chatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatGPTApp.swift
+//  chatGPT
+//
+//  Created by Mac on 19/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct chatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
